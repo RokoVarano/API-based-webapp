791fffaca9f524b149939ee457246f086d51c63e
Alonso Falconi <alonso.fl@live.com>
2021-08-03 13:54:52 -0500

    Add support text to comments section

--- src/frontend/comments.js
+++ src/frontend/comments.js
@@ -14,11 +14,30 @@ const commentsPopUp = () => {
 
   const popUpImage = document.createElement('img');
   popUpImage.className = 'show-image';
-  popUpImage.src = mockAPI[0].show.image.medium
+  popUpImage.src = mockAPI[0].show.image.medium;
+
+  const popUpTitle = document.createElement('h2');
+  popUpTitle.className = 'popup-title';
+  popUpTitle.textContent = mockAPI[0].show.name;
+
+  const popUpSupportContent = document.createElement('div');
+  popUpSupportContent.className = 'popup-support-content';
+
+  const popUpSupportType = document.createElement('h3');
+  popUpSupportType.className = 'popup-support-text'
+  popUpSupportType.textContent = `Show Type: ${mockAPI[0].show.type}`;
+
+  const popUpSupportGenre = document.createElement('h3');
+  popUpSupportGenre.className = 'popup-support-text';
+  popUpSupportGenre.textContent = `Genre: ${mockAPI[0].show.genres[0]}`;
 
   document.body.appendChild(containerBackdrop);
   containerBackdrop.appendChild(commentsSection);
   commentsSection.appendChild(popUpImage);
+  commentsSection.appendChild(popUpTitle);
+  commentsSection.appendChild(popUpSupportContent);
+  popUpSupportContent.appendChild(popUpSupportType);
+  popUpSupportContent.appendChild(popUpSupportGenre);
 };
 
 export default commentsPopUp;
