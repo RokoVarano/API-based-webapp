95ad2e54f0cc1b0227942a720525468ee946aed5
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-05 19:55:19 -0400

    clean for upload

--- src/backend/tvAPI.js
+++
@@ -1,8 +0,0 @@
-const tvAPI = {
-  url: 'https://api.tvmaze.com/search/shows?q=shark',
-  async get() {
-    return fetch(this.url).then((response) => response.json());
-  },
-};
-
-export default tvAPI;
